81dbb1ccb133684274fa44f5d30c575c6a144759
Niv Oridocs <fradige95@gmail.com>
2024-03-22 18:55:38 +0100

    Replace custom recipe with modded dynamic registry

--- src/main/java/niv/flowstone/Flowstone.java
+++ src/main/java/niv/flowstone/Flowstone.java
@@ -4,12 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.registry.DynamicRegistries;
 import net.minecraft.core.Registry;
-import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.crafting.RecipeSerializer;
-import net.minecraft.world.item.crafting.RecipeType;
-import niv.flowstone.recipe.FlowstoneRecipe;
 
 public class Flowstone implements ModInitializer {
     // This logger is used to write text to the console and the log file.
@@ -18,23 +16,14 @@ public class Flowstone implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("Flowstone");
 
     public static final String MOD_ID;
-    public static final RecipeType<FlowstoneRecipe> FLOWSTONE;
-    public static final RecipeSerializer<FlowstoneRecipe> FLOWSTONE_SERIALIZER;
 
-    private static final ResourceLocation ID;
+    public static final ResourceKey<Registry<FlowstoneGenerator>> GENERATOR;
 
     static {
         MOD_ID = "flowstone";
-        ID = new ResourceLocation(MOD_ID, MOD_ID);
-        FLOWSTONE = new RecipeType<FlowstoneRecipe>() {
-            @Override
-            public String toString() {
-                return Flowstone.ID.toString();
-            }
-        };
-        FLOWSTONE_SERIALIZER = new FlowstoneRecipe.Serializer();
-        Registry.register(BuiltInRegistries.RECIPE_TYPE, ID, FLOWSTONE);
-        Registry.register(BuiltInRegistries.RECIPE_SERIALIZER, ID, FLOWSTONE_SERIALIZER);
+
+        GENERATOR = ResourceKey.<FlowstoneGenerator>createRegistryKey(new ResourceLocation(MOD_ID, "generators"));
+        DynamicRegistries.register(GENERATOR, FlowstoneGenerator.CODEC);
     }
 
     @Override
