ee36a7da9b9b6464adf69456ddd3e6825fb0d646
Niv Oridocs <fradige95@gmail.com>
2024-04-20 13:34:20 +0200

    Move default ore generators to active-by-default built-it datapack

--- src/main/java/niv/flowstone/Flowstone.java
+++ src/main/java/niv/flowstone/Flowstone.java
@@ -5,7 +5,12 @@ import org.slf4j.LoggerFactory;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.registry.DynamicRegistries;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
 import net.minecraft.core.Registry;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 
@@ -17,10 +22,14 @@ public class Flowstone implements ModInitializer {
 
     public static final String MOD_ID;
 
+    public static final ModContainer MOD_CONTAINER;
+
     public static final ResourceKey<Registry<FlowstoneGenerator>> GENERATOR;
 
     static {
         MOD_ID = "flowstone";
+
+        MOD_CONTAINER = FabricLoader.getInstance().getModContainer(MOD_ID).orElseThrow();
 
         GENERATOR = ResourceKey.<FlowstoneGenerator>createRegistryKey(new ResourceLocation(MOD_ID, "generators"));
         DynamicRegistries.register(GENERATOR, FlowstoneGenerator.CODEC);
@@ -32,5 +41,15 @@ public class Flowstone implements ModInitializer {
         // However, some things (like resources) may still be uninitialized.
         // Proceed with mild caution.
         LOGGER.info("Initialize");
+
+        registerDefaultDatapack("overworld_ores", "Overworld Ores 1%");
+    }
+
+    private static final void registerDefaultDatapack(String path, String name) {
+        ResourceManagerHelper.registerBuiltinResourcePack(
+                new ResourceLocation(MOD_ID, path),
+                MOD_CONTAINER,
+                Component.literal(name),
+                ResourcePackActivationType.DEFAULT_ENABLED);
     }
 }
